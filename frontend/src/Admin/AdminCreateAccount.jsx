088562455288349f6796de955d1c3eec7600d52d
assasino195 <90238903+assasino195@users.noreply.github.com>
2025-08-07 17:13:40 +0800

    Update AdminCreateAccount.jsx

--- frontend/src/Admin/AdminCreateAccount.jsx
+++ frontend/src/Admin/AdminCreateAccount.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import AdminTopBar from './AdminTopBar';   // <- update path as needed
-import AdminSidebar from './AdminSidebar';
+import AdminSidebar from './AdminSideBar';
 import './AdminCreateAccount.css';
 
 export default function AdminCreateAccount() {
