b2858d4e458e8eb8e284396c8bdef6dbd819573d
assasino195 <90238903+assasino195@users.noreply.github.com>
2025-08-08 13:59:16 +0800

    Update login.jsx

--- frontend/src/Login/login.jsx
+++ frontend/src/Login/login.jsx
@@ -37,7 +37,7 @@ export default function Login() {
 
     try {
       // Call login API
-      const response = await fetch('http://localhost:8000/api/accounts/login', {
+      const response = await fetch('https://fyp-testing-qxwo.onrender.com/api/accounts/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
